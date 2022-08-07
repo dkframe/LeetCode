de7ae217e6c491b7fff89f80ad71ec788be2978f
Dillon <dkframe@ncsu.edu>
2022-08-07 18:51:22 +0200

    first commit

---
+++ LeetCode.cpp
@@ -0,0 +1,12 @@
+﻿// LeetCode.cpp : Defines the entry point for the application.
+//
+
+#include "LeetCode.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}

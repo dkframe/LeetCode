58409893d1c8210bf739bbe195d0602a4778ba09
Dillon <dkframe@ncsu.edu>
2022-10-04 13:37:05 -0400

    Problem 112

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -247,7 +247,45 @@ namespace problem1672
 		}
 	};
 }
+namespace problem112 
+{
 
+	// Definition for a binary tree node.
+	struct TreeNode {
+		int val;
+		TreeNode *left;
+		TreeNode *right;
+		TreeNode() : val(0), left(nullptr), right(nullptr) {}
+		TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+		TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+	};
+ 
+	class Solution {
+	public:
+		bool hasPathSum(TreeNode* root, int targetSum) {
+			if (root == NULL) {return false;}
+
+			targetSum -= (root->val);
+
+			if ( (root->left) == NULL && (root->right) == NULL ) 
+			{
+				return (targetSum == 0);
+			}
+			else if ((root->left) != NULL && (root->right) == NULL)
+			{
+				return hasPathSum(root->left, targetSum);
+			}
+			else if ((root->left) == NULL && (root->right) != NULL)
+			{
+				return hasPathSum(root->right, targetSum);
+			}
+			else
+			{
+				return (hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum));
+			}
+		}
+	};
+}
 
 int main()
 {
